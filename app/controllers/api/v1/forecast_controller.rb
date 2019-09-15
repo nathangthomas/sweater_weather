ff9a18f176f8dd5059aae464f340e77a16934628
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-09-15 10:28:05 -0600

    Add update, create, and destroy methods to API

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -4,8 +4,25 @@ class Api::V1::ForecastController < ApplicationController
     render json: Forecast.all
   end
 
-  # def show
-  #   render json: Forecast.find(params[:id])
-  # end
+  def show
+    render json: Forecast.find(params[:id])
+  end
 
+  def create
+    render json: Forecast.create!(forecast_params)
+  end
+
+  def update
+    render json: Forecast.update(params[:id], forecast_params)
+  end
+
+  def destroy
+    render json: Forecast.delete(params[:id])
+  end
+
+  private
+
+    def forecast_params
+      params.require(:forecast).permit(:city, :state, :country, :temperature, :high, :low, :humidity, :visability, :uv_index)
+    end
 end
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -13,13 +13,50 @@ describe 'Forecast API' do
     expect(forecasts.count).to eq(5)
   end
 
-  # it 'sends forcast information for one city' do
-  #   id = create(:forecast).id
-  #   get "/api/v1/forecast/#{id}"
-  #
-  #   forecast = JSON.parse(response.body)
-  #
-  #   expect(response).to be_successful
-  #   expect(forecast["id"].to eq(id))
-  # end
+  it 'sends forcast information for one city' do
+    id = create(:forecast).id
+    get "/api/v1/forecast/#{id}"
+
+    forecast = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(forecast["id"]).to eq(id)
+  end
+
+  it 'can create a new forecast' do
+    forecast_params = {city: 'Denver', state: 'CO', country: 'USA', temperature: '80', high: 120, low: 65, humidity: 10, visibility: 90, uv_index: 2}
+
+    post "/api/v1/forecast", params: {forecast: forecast_params}
+
+    forecast = Forecast.last
+
+    expect(response).to be_successful
+    expect(forecast.city).to eq(forecast_params[:city])
+  end
+
+  it 'can update an existing forecast' do
+    id = create(:forecast).id
+    previous_city = Forecast.last.city
+    forecast_params = forecast_params = {city: 'Boulder'}
+
+    put "/api/v1/forecast/#{id}", params: {forecast: forecast_params}
+    forecast = Forecast.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(forecast.city).to_not eq(previous_city)
+    expect(forecast.city).to eq('Boulder')
+  end
+
+  it 'can destroy a forecast' do
+
+    forecast = create(:forecast)
+
+    expect(Forecast.count).to eq(1)
+
+    delete "/api/v1/forecast/#{forecast.id}"
+
+    expect(response).to be_successful
+    expect(Forecast.count).to eq(0)
+    expect{Forecast.find(forecast.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
