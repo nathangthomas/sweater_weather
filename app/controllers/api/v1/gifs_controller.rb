e253bf92e63290fe2396b5ed092da843c2d4e67b
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-09-16 11:42:15 -0600

    Add hourly_data to GifsController

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::GifsController < ApplicationController
   def index
-    forecast = ForecastService.new(@coordinates)
+    forecast = ForecastService.new('39.7392358,-104.990251')
+    hourly_data = forecast.forecast_data[:hourly][:data]
 
-    forecast.hourly_summary.map do |summary|
+    hourly_data.map do |hour|
+      summary = hour[:summary]
+      #I realize you said daily summary... I only pulled the hourly summary in my forecast API call. I'll have to go back and fix that.
       response = Faraday.get "https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_API_KEY"]}=#{summary}&limit=1"
       JSON.parse(response.body, symbolize_names: true)
     end
--- spec/requests/api/v1/giphy_request_spec.rb
+++ spec/requests/api/v1/giphy_request_spec.rb
@@ -4,7 +4,7 @@ describe 'GiphyController' do
    it 'sends a list of gifs for each days weather' do
     create_list(:gif, 14)
 
-    # stub_gifs_api_calls
+    stub_gifs_api_calls
 
     stub_request(:get, "https://api.giphy.com/v1/gifs/search?api_key=l1RMz2nBcSsGN8n7HhFAHAdviLXs6nvS&limit=1&q='Mostly%20cloudy%20throughout%20the%20day.'").to_return(status: 200, body: File.read("./fixtures/gif.json"), headers: {})
 
